87373b7ad5e93f5275894be67f8745e452faad63
Md Maruf Uzzaman <mdmarufuzzaman@mac.home>
2025-06-25 15:08:25 -0300

    Fix: Add .gitignore and remove large files from repo

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qtYB9iZ2QhREbKazE3hkHzz3ojM78RkjQcOhVxcn1M4=\"\n}"
--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_0766a282._.js",
-  "static/chunks/src_components_Navbar_tsx_624fc2ba._.js"
-],
-    source: "dynamic"
-});
